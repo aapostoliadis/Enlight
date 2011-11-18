0f78acfc002c627f7cbd4d0f63cef5c147de6128
Heiner Lohaus <hl@shopware.de>
2011-11-18 15:30:37 +0100

    #6200 - Setup commponents test base

---
+++ Tests/Enlight/Tests/Config/AllTests.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Enlight
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://enlight.de/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@shopware.de so we can send you a copy immediately.
+ *
+ * @category   Enlight
+ * @package    Enlight_Tests
+ * @copyright  Copyright (c) 2011, shopware AG (http://www.shopware.de)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ * @version    $Id$
+ * @author     Heiner Lohaus
+ * @author     $Author$
+ */
+
+/**
+ * Test suite
+ *
+ * @category   Enlight
+ * @package    Enlight_Tests
+ * @copyright  Copyright (c) 2011, shopware AG (http://www.shopware.de)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+class Enlight_Tests_Config_AllTests
+{
+	/**
+	 * Returns the test suite
+	 *
+	 * @return PHPUnit_Framework_TestSuite
+	 */
+	public static function suite()
+	{
+		$suite = new Enlight_Components_Test_TestSuite('Enlight Application');
+
+		$testCollector = new PHPUnit_Runner_IncludePathTestCollector(
+			array(dirname(__FILE__))
+		);
+
+		$suite->addTestFiles($testCollector->collectTests());
+
+		return $suite;
+	}
+}
