4a0438b2dcfea59616fc4455d8376adf0bd5fa87
Heiner Lohaus <hl@shopware.de>
2012-01-17 18:03:53 +0100

    #6230 - Add script renderer controller plugin / add a backend auth example

--- Library/Enlight/Controller/Plugins/ScriptRenderer/Bootstrap.php
+++ Library/Enlight/Controller/Plugins/ScriptRenderer/Bootstrap.php
@@ -1,128 +1,127 @@
 <?php
 /**
- * Enlight
- *
- * LICENSE
- *
- * This source file is subject to the new BSD license that is bundled
- * with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://enlight.de/license
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@shopware.de so we can send you a copy immediately.
- *
- * @category   Enlight
- * @package    Enlight_Extensions
- * @copyright  Copyright (c) 2011, shopware AG (http://www.shopware.de)
- * @license    http://enlight.de/license     New BSD License
- * @version    $Id$
- * @author     Heiner Lohaus
- * @author     $Author$
- */
+* Enlight
+*
+* LICENSE
+*
+* This source file is subject to the new BSD license that is bundled
+* with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://enlight.de/license
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@shopware.de so we can send you a copy immediately.
+*
+* @category   Enlight
+* @package    Enlight_Extensions
+* @copyright  Copyright (c) 2011, shopware AG (http://www.shopware.de)
+* @license    http://enlight.de/license     New BSD License
+* @version    $Id$
+* @author     Heiner Lohaus
+* @author     $Author$
+*/
 
 /**
- * @category   Enlight
- * @package    Enlight_Extensions
- * @copyright  Copyright (c) 2011, shopware AG (http://www.shopware.de)
- * @license    http://enlight.de/license     New BSD License
- */
-class Enlight_Controller_Plugins_ScriptRenderer_Bootstrap extends Enlight_Plugin_Bootstrap_Default
+* @category   Enlight
+* @package    Enlight_Extensions
+* @copyright  Copyright (c) 2011, shopware AG (http://www.shopware.de)
+* @license    http://enlight.de/license     New BSD License
+*/
+class Enlight_Controller_Plugins_ScriptRenderer_Bootstrap extends Enlight_Plugin_Bootstrap
 {
     /**
-     * @var string
-     */
+    * @var string
+    */
     protected $target = ':module/:controller/:file:suffix';
 
     /**
-     * @var string
-     */
-    protected $defaultFile = 'app';
+    * @var string
+    */
+    protected $defaultFile = null;
 
     /**
-     * @var array
-     */
+    * @var array
+    */
     protected $filterRules = array(
-        ':module'     => array('Word_CamelCaseToUnderscore','StringToLower'),
-        ':controller' => array('Word_CamelCaseToUnderscore','StringToLower'),
-        ':file'       => array('Word_CamelCaseToUnderscore','StringToLower'),
-        'suffix'      => '.js'
+       ':module'     => array('Word_CamelCaseToUnderscore','StringToLower'),
+       ':controller' => array('Word_CamelCaseToUnderscore','StringToLower'),
+       ':file'       => array('Word_CamelCaseToUnderscore','StringToLower'),
+       'suffix'      => '.js'
     );
 
     /**
-     * @var array
-     */
+    * @var array
+    */
     protected $headers = array('Content-Type' => 'application/javascript;charset=utf-8');
 
     /**
-     * @var bool
-     */
+    * @var bool
+    */
     protected $render = false;
 
     /**
-     * @var Enlight_Controller_Plugins_ViewRenderer_Bootstrap
-     */
+    * @var Enlight_Controller_Plugins_ViewRenderer_Bootstrap
+    */
     protected $viewRenderer;
 
     /**
-	 * Plugin install method
-	 */
-	public function init()
-	{
-        $event = new Enlight_Event_Handler_Default(
-            'Enlight_Controller_Action_PreDispatch',
-            300,
-            array($this, 'onPreDispatch')
-        );
-        $this->Application()->Events()->registerListener($event);
-	}
+     * Plugin install method
+     */
+    public function init()
+    {
+            $event = new Enlight_Event_Handler_Default(
+                'Enlight_Controller_Action_PreDispatch',
+                300,
+                array($this, 'onPreDispatch')
+            );
+            $this->Application()->Events()->registerListener($event);
+    }
 
     /**
-     * Loads the script template, if no set.
-     *
-     * @param   Enlight_Event_EventArgs $args
-     */
+    * Loads the script template, if no set.
+    *
+    * @param   Enlight_Event_EventArgs $args
+    */
     public function onPreDispatch(Enlight_Event_EventArgs $args)
     {
-        if(!$this->render) {
-            return;
-        }
+       if(!$this->render) {
+           return;
+       }
 
-        $this->render = false;
+       $this->render = false;
 
-        if($this->viewRenderer->Action()->View()->hasTemplate()
-          || !$this->viewRenderer->shouldRender()) {
-            return;
-        }
+       if($this->viewRenderer->Action()->View()->hasTemplate()
+         || !$this->viewRenderer->shouldRender()) {
+           return;
+       }
 
-        if(($template = $this->getTemplateName()) !== null) {
-            $this->viewRenderer->Action()->View()->loadTemplate($template);
-        }
-
-        foreach($this->headers as $name => $value) {
-            $this->viewRenderer->Action()->Response()->setHeader($name, $value);
-        }
+       if(($template = $this->getTemplateName()) !== null) {
+           $this->viewRenderer->Action()->View()->loadTemplate($template);
+           foreach($this->headers as $name => $value) {
+               $this->viewRenderer->Action()->Response()->setHeader($name, $value);
+           }
+       }
     }
 
     /**
-     * Sets the render flag.
-     * Loads the view renderer.
-     *
-     * @param   bool $flag
-     * @return  Enlight_Controller_Plugins_ScriptRenderer_Bootstrap
-     */
+    * Sets the render flag.
+    * Loads the view renderer.
+    *
+    * @param   bool $flag
+    * @return  Enlight_Controller_Plugins_ScriptRenderer_Bootstrap
+    */
     public function setRender($flag = true)
     {
-        $this->setViewRenderer();
-        $this->render = $flag ? true : false;;
-        return $this;
+       $this->setViewRenderer();
+       $this->render = $flag ? true : false;;
+       return $this;
     }
 
     /**
-     * Returns the template name.
-     *
-     * @return  string
-     */
+    * Returns the template name.
+    *
+    * @return  string
+    */
     public function getTemplateName()
     {
         $request = $this->viewRenderer->Action()->Request();
@@ -139,13 +138,13 @@ class Enlight_Controller_Plugins_ScriptRenderer_Bootstrap extends Enlight_Plugin
         $fileName = ltrim(dirname($fileName). '/' . basename($fileName, '.js'), '/.');
 
         if(empty($fileName)) {
-            return null;
+           return null;
         }
 
         $templateName = $inflector->filter(array(
-            'module' => $moduleName,
-            'controller' => $controllerName,
-            'file' => $fileName
+           'module' => $moduleName,
+           'controller' => $controllerName,
+           'file' => $fileName
         ));
 
         return $templateName;
