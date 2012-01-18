e12ad3e9fd9063cd6cfcbe2954bdbc1649315eaa
unknown <j.schwehn@SW-PWS-11.shopware.local>
2012-01-18 10:27:34 +0100

    #6744 - Added inline documentation and licence header

--- Library/Enlight/Template/Plugins/function.action.php
+++ Library/Enlight/Template/Plugins/function.action.php
@@ -1,4 +1,37 @@
 <?php
+/**
+ * Enlight
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://enlight.de/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@shopware.de so we can send you a copy immediately.
+ *
+ * @category   Enlight
+ * @package    Enlight_Template_Plugins
+ * @copyright  Copyright (c) 2011, shopware AG (http://www.shopware.de)
+ * @license    http://enlight.de/license     New BSD License
+ * @version    $Id$
+ * @author     Heiner Lohaus
+ * @author     $Author$
+ */
+/**
+ * Method to fast deploy a query to enlight
+ * 
+ * The params array knows the following keys
+ * - name : The name of the action to call
+ * - params : optional params array for the specific action call
+ * 
+ * @param $params
+ * @param Enlight_Template_Default $template
+ * @return string
+ * @throws Exception
+ */
 function smarty_function_action($params, Enlight_Template_Default $template)
 {
     /** @var $front Enlight_Controller_Front */
--- Library/Enlight/Template/Plugins/function.config.php
+++ Library/Enlight/Template/Plugins/function.config.php
@@ -1,4 +1,38 @@
 <?php
+/**
+ * Enlight
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://enlight.de/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@shopware.de so we can send you a copy immediately.
+ *
+ * @category   Enlight
+ * @package    Enlight_Template_Plugins
+ * @copyright  Copyright (c) 2011, shopware AG (http://www.shopware.de)
+ * @license    http://enlight.de/license     New BSD License
+ * @version    $Id$
+ * @author     Heiner Lohaus
+ * @author     $Author$
+ */
+/**
+ * Function to get access to the Enlight2 Config system.
+ * 
+ * The params array knows the key
+ * - name    : Name of the config parameter which should be requested
+ * - default : Default value if the queried config key does not exists
+ * 
+ * @param array $params
+ * @param mixed $smarty
+ * @param string $template
+ * 
+ * @return mixed
+ */
 function smarty_function_config($params, $smarty, $template)
 {
 	if(empty($params['name'])
