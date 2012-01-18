57a1203c992520b345bb337d590d897434b80d78
unknown <j.schwehn@SW-PWS-11.shopware.local>
2012-01-18 10:27:34 +0100

    #6744 - Added inline documentation and licence header

--- Library/Enlight/Template/Plugins/modifier.currency.php
+++ Library/Enlight/Template/Plugins/modifier.currency.php
@@ -1,4 +1,35 @@
 <?php
+/**
+ * Enlight
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://enlight.de/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@shopware.de so we can send you a copy immediately.
+ *
+ * @category   Enlight
+ * @package    Enlight_Template_Plugins
+ * @copyright  Copyright (c) 2011, shopware AG (http://www.shopware.de)
+ * @license    http://enlight.de/license     New BSD License
+ * @version    $Id$
+ * @author     Heiner Lohaus
+ * @author     $Author$
+ */
+/**
+ * Formats a given decimal value to a local aware currency value
+ * 
+ * 
+ * @link http://framework.zend.com/manual/de/zend.currency.options.html
+ * @param float $value Value can have a coma as a decimal separator 
+ * @param array $config
+ * @param string $position where the currency symbol should be displayed
+ * @return float|string
+ */
 function smarty_modifier_currency($value, $config=null, $position=null)
 {
     if(!Enlight_Application::Instance()->Bootstrap()->hasResource('Currency')) {
